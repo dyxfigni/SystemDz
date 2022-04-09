f9d4d46e35a2a789159d22cdc8e1d27858a87582
dyxfigni <lack230206@gmail.com>
2022-04-09 18:50:51 +0300

    18 50 не записівает 2 и 3 поток

--- SystemDz/Form1.cs
+++ SystemDz/Form1.cs
@@ -17,13 +17,14 @@ namespace SystemDz
     public partial class Form1 : Form
     {
         private static Mutex mutex;
-        private static int[] array;
-        static XmlSerializer xmlSerializer = new XmlSerializer(typeof(int[]));
+        private static List<int> numbersList;
+        static XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<int>));
+        static int k = new Random().Next(50, 200);
 
         public Form1()
         {
             InitializeComponent();
-            array = new int[new Random().Next(50, 200)];
+            numbersList = new List<int>();
             mutex = new Mutex();
         }
 
@@ -37,16 +38,8 @@ namespace SystemDz
             thread1.Start();
             thread1.Join();
 
-            for (int i = 0; i < array.Length; i++)
-            {
-                lstBox1.Items.Add(array[i]);
-                Thread.Sleep(50);
-            }
-
-
             thread2.Start();
             thread2.Join();
-
 
             thread3.Start();
             thread3.Join();
@@ -61,15 +54,18 @@ namespace SystemDz
         static void GenRandNum()
         {
             mutex.WaitOne();
-            for (int i = 0; i < array.Length; i++)
+            Random random = new Random();
+
+            for (int i = 0; i < k; i++)
             {
-                array[i] += new Random().Next(0, 1000);
+                numbersList.Add(random.Next(0, 1000));
+
             }
 
             //запись в файл
             using (StreamWriter writer = new StreamWriter("Text.xml", false))
             {
-                xmlSerializer.Serialize(writer, array);
+                xmlSerializer.Serialize(writer, numbersList);
             }
 
             mutex.ReleaseMutex();
@@ -78,16 +74,15 @@ namespace SystemDz
         static void SimpleNum()
         {
             // чтение из файла
-            int[] buffer;
+            List<int> buffer;
             using (StreamReader reader = new StreamReader("Text.xml"))
             {
-                buffer = (int[])xmlSerializer.Deserialize(reader);
+                buffer = (List<int>)xmlSerializer.Deserialize(reader);
             }
 
-            int[] temp = new int[buffer.Length];
-            
+            List<int> temp = new List<int>();
             //простое число
-            for (int i = 0; i < buffer.Length; i++)
+            for (int i = 0; i < buffer.Count; i++)
             {
                 if (buffer[i] > 1)
                 {
@@ -95,12 +90,12 @@ namespace SystemDz
                     {
                         if (buffer[i] % 1 != 0)
                         {
-                            temp[j] = buffer[i];
+                            temp.Add(buffer[i]);
                         }
                     }
                 }
             }
-
+            buffer.Clear();
             buffer = null;
 
             // запись в файл
@@ -108,39 +103,51 @@ namespace SystemDz
             {
                 xmlSerializer.Serialize(writer, temp);
             }
+            temp.Clear();
+            temp = null;
         }
 
         static void WhereNum7()
         {
             // чтение из файла
-            int[] buffer;
+            List<int> buffer;
             using (StreamReader reader = new StreamReader("TextSimple.xml"))
             {
-                buffer = (int[])xmlSerializer.Deserialize(reader);
+                buffer = (List<int>)xmlSerializer.Deserialize(reader);
             }
 
-            int[] temp = new int[buffer.Length];
+            List<int> temp = new List<int>();
 
             // если число оканчивается на 7
-            for (int i = 0; i < buffer.Length; i++)
+            for (int i = 0; i < buffer.Count; i++)
             {
-                for (int j = 0; j < temp.Length; j++)
+                for (int j = 0; j < temp.Count; j++)
                 {
                     if ((buffer[i] % 10) == 7)
                     {
-                        temp[j] = buffer[i];
+                        temp.Add(buffer[i]);
                     }
                 }
             }
+
+            buffer.Clear();
+            buffer = null;
 
             // запись в файл
             using (StreamWriter writer = new StreamWriter("TextSimpleNum7.xml", false))
             {
                 xmlSerializer.Serialize(writer, temp);
             }
+            temp.Clear();
+            temp = null;
         }
 
     private void lstBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
