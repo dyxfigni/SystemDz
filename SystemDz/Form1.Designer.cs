2b2ca3435cbeca01bcd1e6994013a81bdcf29117
dyxfigni <lack230206@gmail.com>
2022-04-09 13:37:52 +0300

    Добавьте файлы проекта.

---
+++ SystemDz/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace SystemDz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstBox1 = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstBox2 = new System.Windows.Forms.ListBox();
+            this.lstBox3 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstBox1
+            // 
+            this.lstBox1.FormattingEnabled = true;
+            this.lstBox1.Location = new System.Drawing.Point(12, 112);
+            this.lstBox1.Name = "lstBox1";
+            this.lstBox1.Size = new System.Drawing.Size(201, 264);
+            this.lstBox1.TabIndex = 0;
+            this.lstBox1.SelectedIndexChanged += new System.EventHandler(this.lstBox1_SelectedIndexChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(667, 126);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 41);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstBox2
+            // 
+            this.lstBox2.FormattingEnabled = true;
+            this.lstBox2.Location = new System.Drawing.Point(219, 112);
+            this.lstBox2.Name = "lstBox2";
+            this.lstBox2.Size = new System.Drawing.Size(201, 264);
+            this.lstBox2.TabIndex = 3;
+            // 
+            // lstBox3
+            // 
+            this.lstBox3.FormattingEnabled = true;
+            this.lstBox3.Location = new System.Drawing.Point(426, 112);
+            this.lstBox3.Name = "lstBox3";
+            this.lstBox3.Size = new System.Drawing.Size(201, 264);
+            this.lstBox3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 33);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "First Threat";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(205, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 33);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Second Threat";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(433, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(183, 33);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "ThirdThreat";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 415);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(615, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(667, 173);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 41);
+            this.btnStop.TabIndex = 9;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstBox3);
+            this.Controls.Add(this.lstBox2);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lstBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstBox2;
+        private System.Windows.Forms.ListBox lstBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ SystemDz/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace SystemDz
+{
+
+    public partial class Form1 : Form
+    {
+        private static Mutex mutex;
+        private static int[] array;
+        static XmlSerializer xmlSerializer = new XmlSerializer(typeof(int[]));
+
+        public Form1()
+        {
+            InitializeComponent();
+            array = new int[new Random().Next(50, 200)];
+            mutex = new Mutex();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStop.Enabled = true;
+            btnStart.Enabled = false;
+            Thread thread1 = new Thread(GenRandNum);
+            Thread thread2 = new Thread(SimpleNum);
+            Thread thread3 = new Thread(WhereNum7);
+            thread1.Start();
+            thread1.Join();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                lstBox1.Items.Add(array[i]);
+                Thread.Sleep(50);
+            }
+
+
+            thread2.Start();
+            thread2.Join();
+
+
+            thread3.Start();
+            thread3.Join();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            btnStop.Enabled = false;
+            btnStart.Enabled = true;
+        }
+
+        static void GenRandNum()
+        {
+            mutex.WaitOne();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] += new Random().Next(0, 1000);
+            }
+
+            //запись в файл
+            using (StreamWriter writer = new StreamWriter("Text.xml", false))
+            {
+                xmlSerializer.Serialize(writer, array);
+            }
+
+            mutex.ReleaseMutex();
+        }
+
+        static void SimpleNum()
+        {
+            // чтение из файла
+            int[] buffer;
+            using (StreamReader reader = new StreamReader("Text.xml"))
+            {
+                buffer = (int[])xmlSerializer.Deserialize(reader);
+            }
+
+            int[] temp = new int[buffer.Length];
+            
+            //простое число
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                if (buffer[i] > 1)
+                {
+                    for (int j = 2; j < buffer[i]; j++)
+                    {
+                        if (buffer[i] % 1 != 0)
+                        {
+                            temp[j] = buffer[i];
+                        }
+                    }
+                }
+            }
+
+            buffer = null;
+
+            // запись в файл
+            using (StreamWriter writer = new StreamWriter("TextSimple.xml", false))
+            {
+                xmlSerializer.Serialize(writer, temp);
+            }
+        }
+
+        static void WhereNum7()
+        {
+            // чтение из файла
+            int[] buffer;
+            using (StreamReader reader = new StreamReader("TextSimple.xml"))
+            {
+                buffer = (int[])xmlSerializer.Deserialize(reader);
+            }
+
+            int[] temp = new int[buffer.Length];
+
+            // если число оканчивается на 7
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    if ((buffer[i] % 10) == 7)
+                    {
+                        temp[j] = buffer[i];
+                    }
+                }
+            }
+
+            // запись в файл
+            using (StreamWriter writer = new StreamWriter("TextSimpleNum7.xml", false))
+            {
+                xmlSerializer.Serialize(writer, temp);
+            }
+        }
+
+    private void lstBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
