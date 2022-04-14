fff287f94434e5126dcc11f2ba8da6d36edeab29
dyxfigni <lack230206@gmail.com>
2022-04-14 11:18:59 +0200

    Добавил вывод на экран пар

--- SortingNumbersByEventsDz/Form1.Designer.cs
+++ SortingNumbersByEventsDz/Form1.Designer.cs
@@ -123,7 +123,6 @@
             this.Controls.Add(this.btnStart);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- SortingNumbersByEventsDz/Form1.cs
+++ SortingNumbersByEventsDz/Form1.cs
@@ -7,32 +7,92 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace SortingNumbersByEventsDz
 {
     public partial class Form1 : Form
     {
-        static AutoResetEvent myEvent = new AutoResetEvent(false);
-        private static List<int> numberList = new List<int>();
+        static AutoResetEvent myEvent = new AutoResetEvent(true);
+        private static Dictionary<int, int> pair = new Dictionary<int, int>();
+        private static XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<int>));
+
         public Form1()
         {
             InitializeComponent();
 
-            lst
-        }
+            btnStop.Enabled = false;
+            btnStart.Enabled = true;
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
+            lstBox1.Items.Clear();
+            lstBox2.Items.Clear();
+            lstBox3.Items.Clear();
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
+            Thread thread1 = new Thread(GenNumForPairs);
+            Thread thread2 = new Thread(GetSumInPairs);
+            Thread thread3 = new Thread(GetProductInPairs);
 
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+
+            MessageBox.Show("Completed", "Message");
+
+            FillMyListBox(lstBox1);
         }
 
+        static void GenNumForPairs()
+        {
+            myEvent.WaitOne();
+            Random random = new Random();
+
+            //Dictionary<int, int> pair = new Dictionary<int, int>();
+
+            for (int i = 0; i < new Random().Next(50, 200); i++)
+            {
+                int key = random.Next(0, 1000);
+                while(pair.ContainsKey(key))
+                {
+                    key = random.Next(0, 1000);
+                }
+
+                pair.Add(key, random.Next(0, 1000));
+            }
+
+            myEvent.Reset();
+        }
+
+        static void GetSumInPairs()
+        {
+            myEvent.WaitOne();
+            int Sum = 0;
+
+            foreach (KeyValuePair<int, int> KeyValuePair in pair)
+            {
+                Sum = KeyValuePair.Value + KeyValuePair.Key;
+            }
+
+            myEvent.Reset();
+        }
+
+        static void GetProductInPairs()
+        {
+            myEvent.WaitOne();
+
+            int Product = 0;
+
+            foreach (KeyValuePair<int, int> KeyValuePair in pair)
+            {
+                Product = KeyValuePair.Value * KeyValuePair.Key;
+            }
+
+            myEvent.Reset();
+        }
 
         private void FillMyListBox(ListBox myListBox, string path)
         {
@@ -52,5 +112,17 @@ namespace SortingNumbersByEventsDz
             temp.Clear();
             temp = null;
         }
+        private void FillMyListBox(ListBox myListBox)
+        {
+            //Полная чистка перед добавлением элементов   
+            myListBox.Items.Clear();
+            //List<int> temp;
+
+            foreach (KeyValuePair<int, int> KeyValuePair in pair)
+            {
+                myListBox.Items.Add($"First: {KeyValuePair.Key} Second: {KeyValuePair.Value}");
+            }
+        }
     }
 }
+
