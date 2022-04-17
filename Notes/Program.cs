2dd471ba0135ef32de9a25f1c7b0c24190a29a31
dyxfigni <lack230206@gmail.com>
2022-04-17 14:49:06 +0200

    Заметки

---
+++ Notes/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Mime;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Notes
+{
+    internal class Program
+    {
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+        const int SW_Min = 2;
+        const int SW_Max = 3;
+        const int SW_Norm = 4;
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static HookProc proc = HookCallback;
+        private static IntPtr hook = IntPtr.Zero;
+        static void Main(string[] args)
+        {
+            ShowWindow(GetConsoleWindow(), SW_HIDE);
+            hook = SetHook(proc);
+            Application.Run();
+            UnhookWindowsHookEx(hook);
+            
+
+        }
+        private static IntPtr SetHook(HookProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+        private delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if ((nCode >= 0) && (wParam == (IntPtr)WM_KEYDOWN))
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                if (((Keys)vkCode == Keys.NumPad1) || ((Keys)vkCode == Keys.NumPad0))
+                {
+                    //MessageBox.Show("Note");
+                    ShowWindow(GetConsoleWindow(), SW_SHOW);
+
+                    Console.WriteLine("{0} blocked!", (Keys)vkCode);
+                    return (IntPtr)1;
+                }
+                Console.WriteLine("Press any key to continue:");
+                Console.ReadKey();
+                ShowWindow(GetConsoleWindow(), SW_HIDE);
+            }
+            return CallNextHookEx(hook, nCode, wParam, lParam);
+        }
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    }
+}
